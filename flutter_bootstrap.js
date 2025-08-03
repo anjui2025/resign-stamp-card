177047d303cb49d2ebc19a9763c5488e50922f11
anjui2025 <caramel.unit@gmail.com>
2025-08-04 01:26:44 +0800

    新增快樂集點卡內容顯示

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1542987307"
+    serviceWorkerVersion: "2096406001"
   }
 });
