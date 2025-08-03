d5b4f6798ba0d2c977217a992d6b53a047db10e6
anjui2025 <caramel.unit@gmail.com>
2025-08-04 01:33:02 +0800

    緊急修正build根目錄錯誤

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2096406001"
+    serviceWorkerVersion: "3783074666"
   }
 });
